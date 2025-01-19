479a7af672f865021ea498a7e7742288c9057735
Group18 <23130056@st.hcmuaf.edu.vn>
2025-01-19 14:54:02 +0700

    Add files via upload

---
+++ assets/js/all.js
@@ -0,0 +1,55 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    
+    // Nếu menu đang ẩn, set lại opacity để có thể hiển thị lại menu từ từ
+    if (!menu.classList.contains("show")) {
+      menu.style.opacity = 0; // Đảm bảo opacity là 0 khi bắt đầu ẩn
+      setTimeout(function() {
+        menu.classList.add("show"); // Thêm lớp show để menu trượt vào
+        menu.style.opacity = 1; // Đưa opacity về 1 khi menu xuất hiện
+      }, 10); // Đảm bảo thời gian chờ ngắn để hiệu ứng được kích hoạt
+    } else {
+      // Nếu menu đã hiển thị, chúng ta sẽ giảm opacity trước khi ẩn
+      menu.style.opacity = 0;
+      setTimeout(function() {
+        menu.classList.remove("show"); // Gỡ bỏ lớp show để menu biến mất
+      }, 500); // Đợi 0.5 giây trước khi ẩn hoàn toàn
+    }
+  }
+  
+  // Ẩn menu khi nhấn ra ngoài
+  document.addEventListener("click", function (event) {
+    var menu = document.getElementById("menu");
+    var iconHamburger = document.querySelector(".icon_hamburger");
+  
+    // Kiểm tra xem có phải nhấn ra ngoài menu không và nếu có thì ẩn menu
+    if (
+      menu.classList.contains("show") &&
+      !menu.contains(event.target) &&
+      !iconHamburger.contains(event.target)
+    ) {
+      menu.style.opacity = 0; // Tạo hiệu ứng mờ dần
+      setTimeout(function () {
+        menu.classList.remove("show"); // Ẩn menu sau khi opacity giảm dần
+      }, 500); // Chờ hiệu ứng opacity kết thúc trước khi ẩn menu
+    }
+  });
+  
+  // Hàm hiển thị hoặc ẩn thanh tìm kiếm và overlay
+  function toggleSearchBar(event) {
+    event.preventDefault(); // Ngăn chặn hành động mặc định của thẻ <a>
+    
+    // Lấy các phần tử thanh tìm kiếm và overlay
+    const searchBar = document.getElementById('searchBar');
+    const overlay = document.getElementById('overlay');
+    
+    // Kiểm tra trạng thái hiển thị hiện tại và chuyển đổi nó
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+      searchBar.style.display = 'block'; // Hiển thị thanh tìm kiếm
+      overlay.style.display = 'block'; // Hiển thị overlay làm mờ nền
+    } else {
+      searchBar.style.display = 'none'; // Ẩn thanh tìm kiếm
+      overlay.style.display = 'none'; // Ẩn overlay làm mờ nền
+    }
+  }
+  
